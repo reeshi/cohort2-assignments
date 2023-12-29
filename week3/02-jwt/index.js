c5b1e0fe2f289dc26ee7b0e74854aa304c0be86e
Rishikesh Yadav <yadavrishikesh53@gmail.com>
2023-12-29 21:14:38 +0530

    completed week3/02-jwt

---
+++ week3/02-jwt/index.js
@@ -0,0 +1,72 @@
+const jwt = require('jsonwebtoken');
+const z = require('zod');
+const jwtPassword = 'secret';
+
+
+/**
+ * Generates a JWT for a given username and password.
+ *
+ * @param {string} username - The username to be included in the JWT payload.
+ *                            Must be a valid email address.
+ * @param {string} password - The password to be included in the JWT payload.
+ *                            Should meet the defined length requirement (e.g., 6 characters).
+ * @returns {string|null} A JWT string if the username and password are valid.
+ *                        Returns null if the username is not a valid email or
+ *                        the password does not meet the length requirement.
+ */
+const emailSchema = z.string().email({message: "Invalid email address"});
+const passwordSchema = z.string().min(6);
+
+function signJwt(username, password) {
+    // Your code here
+    const emailResponse = emailSchema.safeParse(username);
+    const passwordResponse = passwordSchema.safeParse(password);
+    if(!emailResponse.success || !passwordResponse.success){
+        return null;
+    }
+    const token = jwt.sign({username}, jwtPassword)
+    return token;
+}
+
+/**
+ * Verifies a JWT using a secret key.
+ *
+ * @param {string} token - The JWT string to verify.
+ * @returns {boolean} Returns true if the token is valid and verified using the secret key.
+ *                    Returns false if the token is invalid, expired, or not verified
+ *                    using the secret key.
+ */
+function verifyJwt(token) {
+    // Your code here
+    try {
+        const decode = jwt.verify(token, jwtPassword);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+/**
+ * Decodes a JWT to reveal its payload without verifying its authenticity.
+ *
+ * @param {string} token - The JWT string to decode.
+ * @returns {object|false} The decoded payload of the JWT if the token is a valid JWT format.
+ *                         Returns false if the token is not a valid JWT format.
+ */
+function decodeJwt(token) {
+    // Your code here
+    const decode = jwt.decode(token);
+    if(decode){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+module.exports = {
+  signJwt,
+  verifyJwt,
+  decodeJwt,
+  jwtPassword,
+};
