e130a5e95e4542131cd13cc1b10b8a3329c45463
Rishikesh Yadav <yadavrishikesh53@gmail.com>
2023-12-29 15:00:56 +0530

    completed week3/01-middlewares

---
+++ week3/01-middlewares/tests/02-ratelimitter.spec.js
@@ -0,0 +1,50 @@
+const app = require("../02-ratelimitter");
+
+const request = require('supertest');
+const assert = require('assert');
+
+beforeAll(() => {
+  jest.setTimeout(20000); // 15 seconds timeout for all tests in this file
+});
+
+describe('GET /user', function () {
+  const userId = 'testId'
+  it('One request responds back correctly', function(done) {
+    request(app)
+      .get('/user')
+      .set('user-id', userId)
+      .then((response) => {
+        expect(response.status).toBe(200);
+        done();
+      });
+  });
+
+  it('5 or more requests return back a 404', function(done) {
+      for (let i = 0; i<5; i++) {
+        request(app).get('/user').set('user-id', userId).then();
+      }
+      request(app)
+        .get('/user')
+        .set('user-id', userId)
+        .then((response) => {
+          expect(response.status).toBe(404);
+          done();
+        });
+  });
+
+  it('5 or more requests and waiting returns a 200', function(done) {
+      for (let i = 0; i<5; i++) {
+        request(app).get('/user').set('user-id', userId).then();
+      }
+      setTimeout(function() {
+      request(app)
+        .get('/user')
+        .set('user-id', userId)
+        .then((response) => {
+          expect(response.status).toBe(200);
+          done();
+        });
+      }, 2000);
+  });
+});
+
